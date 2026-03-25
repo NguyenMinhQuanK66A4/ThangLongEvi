bacd1214d135d195e44fa0f659d61b6019fc0e80
NguyenMinhQuanK66A4 <148624103+NguyenMinhQuanK66A4@users.noreply.github.com>
2026-03-25 16:09:37 +0700

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders landing page heading', () => {
-  render(<App />);
-  expect(screen.getByText(/Thi công Trạm xử lý nước thải quy mô nhỏ/i)).toBeInTheDocument();
-});
